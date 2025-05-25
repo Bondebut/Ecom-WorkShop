34029a46f8eb840a74844015de5bcbef75a6715c
Bonsai Phawakhot <bonza1230@gmail.com>
2025-05-25 22:57:55 +0700

    remove cate

--- server/controllers/cateController.js
+++ server/controllers/cateController.js
@@ -31,12 +31,34 @@ exports.list = async (req, res) => {
 exports.remove = async (req, res) => {
     try {
         const { id } = req.params
-        const category = await prisma.category.delete({
-            where: {
-                id: Number(id)
-            }
+        const categoryId = Number(id)
+         // ตรวจสอบว่ามีหมวดหมู่ "ไม่มีหมวดหมู่" หรือไม่
+        let defaultCategory = await prisma.category.findFirst({
+            where: { name: "ไม่มีหมวดหมู่" }
         })
-        res.send(category)
+        
+        // ถ้าไม่มีหมวดหมู่ "ไม่มีหมวดหมู่" ให้สร้างใหม่
+        if (!defaultCategory) {
+            defaultCategory = await prisma.category.create({
+                data: { name: "ไม่มีหมวดหมู่" }
+            })
+        }
+        
+        // ย้ายผลิตภัณฑ์ทั้งหมดในหมวดหมู่นี้ไปยังหมวดหมู่ "ไม่มีหมวดหมู่"
+        await prisma.product.updateMany({
+            where: { categoryId: categoryId },
+            data: { categoryId: defaultCategory.id }
+        })
+        
+        // ลบหมวดหมู่
+        const category = await prisma.category.delete({
+            where: { id: categoryId }
+        })
+        
+        res.send({ 
+            message: `Category deleted successfully. All products moved to "${defaultCategory.name}" category.`,
+            category 
+        })
     } catch (err) {
         //err
         console.log(err)
